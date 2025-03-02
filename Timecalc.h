79e9b273f4fd376a83cc0f968176b56ab9a4264f
Alexander Klasen <Alexander.klasen23@gmail.com>
2025-03-03 00:03:21 +0100

    Implementation of a days until Expiration Calculator and slight changes in the Main file

---
+++ Timecalc.h
@@ -0,0 +1,16 @@
+#ifndef TIMECALC_H
+#define TIMECALC_H
+#include <ctime>
+
+using std::tm;
+
+
+class Timecalc {
+public:
+
+    int daysuntilexpiration(int d, int m, int y);
+};
+
+
+
+#endif //TIMECALC_H
