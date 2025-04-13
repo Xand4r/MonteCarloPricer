dceaf890d6829400264dca582631e521d48ef673
Xand4r <Alexander.klasen23@gmail.com>
2025-04-13 16:03:43 +0200

    function name bug fix in Main file and Interpolation.cpp

---
+++ cmake-build-debug/CMakeFiles/MonteCarloPricer.dir/cmake_clean.cmake
@@ -0,0 +1,21 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MonteCarloPricer.dir/API_Acc.cpp.o"
+  "CMakeFiles/MonteCarloPricer.dir/API_Acc.cpp.o.d"
+  "CMakeFiles/MonteCarloPricer.dir/Interpolation.cpp.o"
+  "CMakeFiles/MonteCarloPricer.dir/Interpolation.cpp.o.d"
+  "CMakeFiles/MonteCarloPricer.dir/MonteCarlo.cpp.o"
+  "CMakeFiles/MonteCarloPricer.dir/MonteCarlo.cpp.o.d"
+  "CMakeFiles/MonteCarloPricer.dir/Payoff.cpp.o"
+  "CMakeFiles/MonteCarloPricer.dir/Payoff.cpp.o.d"
+  "CMakeFiles/MonteCarloPricer.dir/Timecalc.cpp.o"
+  "CMakeFiles/MonteCarloPricer.dir/Timecalc.cpp.o.d"
+  "CMakeFiles/MonteCarloPricer.dir/main.cpp.o"
+  "CMakeFiles/MonteCarloPricer.dir/main.cpp.o.d"
+  "MonteCarloPricer"
+  "MonteCarloPricer.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MonteCarloPricer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
